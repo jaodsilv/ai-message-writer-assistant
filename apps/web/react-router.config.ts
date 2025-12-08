b2a0a7390d7c24808ee75bdabbed012dfc3b0170
Joao Marco Maciel da Silva <joao@joaodasilva.com.br>
2025-12-07 18:05:42 -0800

    chore(web): add react-router config and tsconfig.app, update gitignore

---
+++ apps/web/react-router.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "@react-router/dev/config";
+
+export default {
+    // Config options...
+    // Server-side rendering (SSR) is enabled by default.
+    // To disable SSR, set `ssr: false`.
+    ssr: true,
+} satisfies Config;
